c37f12c9a7dc8e9bf45c00dd9193dfb0fe762040
Abdurrahman Elkousy <abdelrahman_omar_1515@yahoo.com>
2019-02-12 21:01:46 +0200

    modifications for heroku

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const path = require('path')
 const publicPath = path.join(__dirname,'../public')
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`server is up on port ${port}!`))
